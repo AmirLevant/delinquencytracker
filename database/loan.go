7e837408f67d54f4d93df842db43d7dc95fc5ab5
Amir <amircan206@gmail.com>
2025-10-05 12:20:09 +0300

    initial creation, the beginning additions of how user and loan should look like

---
+++ database/loan.go
@@ -0,0 +1,5 @@
+package database
+
+type Loan struct {
+	id string
+}
---
+++ database/user.go
@@ -0,0 +1,9 @@
+package database
+
+type Person struct {
+	name      string
+	email     string
+	phone     string
+	totalLoan float64
+	loanId    string
+}
