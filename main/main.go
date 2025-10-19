8b332d7995bf5cc4b258d02e0ad548da701c517c
Amir <amircan206@gmail.com>
2025-10-19 15:28:06 +0300

    corrected dbops, testing main connection to postgres database

--- main/main.go
+++ main/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/amirlevant/delinquencytracker/dbconnection"
 )
 
 func main() {
@@ -10,4 +13,21 @@ func main() {
 	var printy string = currentTime.Format(time.DateOnly)
 	fmt.Println("the date is ", printy)
 
+	config := dbconnection.DBConfig{
+		Host:     "localhost",
+		Port:     5432,
+		User:     "postgres",
+		Password: "amir",
+		DBName:   "loan_tracker",
+	}
+
+	db, err := dbconnection.ConnectDB(config)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer dbconnection.CloseDB(db)
+
+	fmt.Println("Success! connected to the database")
+	fmt.Println("Database: loan_tracker")
+	fmt.Println("Host: localhost:5432")
 }
