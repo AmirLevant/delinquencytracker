5ca193b6f8ffdf3cdbb2a3d2d475e9005b2a6c04
Amir <amircan206@gmail.com>
2025-11-04 11:09:56 +0200

    CreatePayment() test created and successfully passed

--- db_test.go
+++ db_test.go
@@ -517,3 +517,36 @@ func TestDeleteLoan(t *testing.T) {
 	require.Empty(t, checkLn)
 
 }
+
+func TestCreatePayment(t *testing.T) {
+	db := setupTestDB(t)
+	defer teardownTestDB(db)
+	dateTaken := time.Now().UTC().Truncate(24 * time.Hour)
+
+	// Arrange
+
+	usr, err := CreateUser(db, "Loan User", "loanuser@test.com", "555-1234")
+	if err != nil {
+		t.Fatalf("Failed to create test user: %v", err)
+	}
+
+	// Creating a loan for the test user
+	ln, err := CreateLoan(db, usr.ID, 10000.00, 0.05, 16, 05, "active", dateTaken)
+	if err != nil {
+		t.Fatalf("CreateLoan failed: %v", err)
+	}
+
+	// Set up payment dates
+	dueDate := dateTaken.Add(30 * 24 * time.Hour) // 30 days after loan was taken
+	paidDate := dueDate.Add(-2 * 24 * time.Hour)  // paid 2 days before due date
+
+	pyment, err := CreatePayment(db, ln.ID, 1, 1000, 900, dueDate, paidDate)
+	if err != nil {
+		t.Fatalf("Create Payment failed %v:", err)
+	}
+
+	var expectedPyment = payment{pyment.ID, ln.ID, 1, 1000, 900, dueDate, paidDate, pyment.CreatedAt}
+
+	require.Equal(t, expectedPyment, pyment)
+
+}
